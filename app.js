14b3dd1bc4f9b4d9b9640a950dc1c049f720b438
Sakshi <sakshis.1407@gmail.com>
2020-11-16 15:15:25 +0530

    Added isLoggedIn middleware

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret",function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
    res.render("secret"); 
 });
 
@@ -71,6 +71,13 @@ app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function(){
     console.log("Server started..");
